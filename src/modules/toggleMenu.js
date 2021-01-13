20d596677236e6dfff8d7dbfbba1903f1e168809
Danila Sokolov <matsubus@mail.ru>
2021-01-13 12:51:55 +0300

    dev(Diploma): added smooth scroll to header from footer

---
+++ src/modules/toggleMenu.js
@@ -0,0 +1,63 @@
+const toggleMenu = () => {
+    const popupMenu = document.querySelector('.popup-menu'),
+        popupDialogMenu = popupMenu.querySelector('.popup-dialog-menu'),
+        menuButton = document.querySelector('.menu'),
+        popupCloseButton = popupMenu.querySelector('.close-menu'),
+        footerUpButton = document.querySelector('.button-footer');
+
+    let open = false;
+
+    const openMenu = () => {
+        popupMenu.style.visibility = 'visible';
+        popupDialogMenu.style.transform = "translate3d(0,0,0)";
+        open = !open;
+    };
+
+    const closeMenu = () => {
+        popupMenu.style.visibility = "";
+        popupDialogMenu.style.transform = "";    
+        open = !open;
+    };
+
+    const popupMenuToggler = () => {
+        if (!open) {
+            openMenu();
+        } else {
+            closeMenu();
+        }
+    };
+
+    popupMenu.addEventListener('click', event => {
+        const target = event.target;
+
+        if (target.matches('.menu-link')) {
+            event.preventDefault();
+            const anchor = document.querySelector(target.getAttribute('href'));
+            console.log(anchor);
+            anchor.scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest'
+            });
+            popupMenuToggler();
+        } else if (target === popupCloseButton) {
+            popupMenuToggler();
+        }
+    });
+
+    menuButton.addEventListener('click', () => {
+        popupMenuToggler();
+    });
+
+    footerUpButton.addEventListener('click', event => {
+        const innerLink = footerUpButton.querySelector('a');
+        const hrefItem = document.querySelector(innerLink.getAttribute('href'));
+
+        event.preventDefault();
+        hrefItem.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest'
+        });
+    }); 
+};
+
+export default toggleMenu;
