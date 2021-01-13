1a5f6cff4676d1cd1fd7d49c19a70349d1e890df
Danila Sokolov <matsubus@mail.ru>
2021-01-13 13:55:42 +0300

    dev(Diploma): added mask for phone inputs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import additionalNumber from './modules/additionalNumber';
 import toggleMenu from './modules/toggleMenu';
 import allServicesPopup from './modules/allServicesPopup';
+import phoneMask from './modules/phoneMask';
 
 additionalNumber();
 toggleMenu();
 allServicesPopup();
+phoneMask();
